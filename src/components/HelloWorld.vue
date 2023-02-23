d9c0fb2914b37cff559b50cf6e8e344df1980574
zhenghemin <zhenghemin@rr.tv>
2023-02-23 20:46:16 +0800

    初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import http from '@/utils/http'
+
+defineProps<{ msg: string }>()
+
+
+const handleGet = async () => {
+   const res = await http.get('/user')
+   console.log(res)
+}
+</script>
+
+<template>
+  <div>
+    <button @click="handleGet">发起get请求</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
